1440b1e49b4fe390ea590b1c9112f7406eb750ee
Chirag <chiragmehta1010@gmail.com>
2021-08-20 13:14:04 +0530

    reff

--- Assignment-1/code/code.py
+++ Assignment-1/code/code.py
@@ -5,10 +5,12 @@ Assignment-1 Vectors
 '''
 import numpy as np
 import matplotlib.pyplot as plt
+from sympy.matrices import Matrix
 x=np.array([1,2,-2])
 y=np.array([5,3,-11])
-
-
+M=Matrix([[1, -2], [-3, -16]])
+M_rref = M.rref()
+print("The reff of matrix M is given as : ",M_rref)
 fig,ax=plt.subplots()
 for i in range(3):
     ax.scatter(x[i],y[i])
@@ -19,6 +21,6 @@ plt.plot(np.array([1,-2]), np.array([5,-11]),'g', label="$CA$")
 plt.grid()
 plt.xlim(-3,3)
 plt.ylim(-14,7)
-
 plt.legend(loc='lower right')
 plt.show()
+
