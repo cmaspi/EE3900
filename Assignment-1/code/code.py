0174bad21206c9506e3bc9196f77fe134b233975
Chirag <chiragmehta1010@gmail.com>
2021-08-20 10:15:21 +0530

    grid added

--- Assignment-1/code/code.py
+++ Assignment-1/code/code.py
@@ -16,6 +16,7 @@ for i in range(3):
 plt.plot(np.array([1,2]),np.array([5,3]), 'b', label="$AB$")
 plt.plot(np.array([2,-2]), np.array([3,-11]),'r', label="$BC$")
 plt.plot(np.array([1,-2]), np.array([5,-11]),'g', label="$CA$")
+plt.grid()
 plt.xlim(-3,3)
 plt.ylim(-14,7)
 
