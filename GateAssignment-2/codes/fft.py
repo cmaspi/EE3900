e28a22706d18f99550b1cde7206f2d27a522301b
cmaspi <chiragmehta1010@gmail.com>
2021-10-18 12:17:01 +0530

    added matrix operations done while using fft

--- GateAssignment-2/codes/fft.py
+++ GateAssignment-2/codes/fft.py
@@ -1,7 +1,20 @@
 from scipy.fft import fft
-
+import numpy as np
+import matplotlib.pyplot as plt
 # input signal
 x= [1,0,2,3]
 # fast fourier transform
 X=fft(x)
+plt.title("Magnitude of $X(k)$")
+plt.xlabel("$k$")
+plt.ylabel("$|X(k)|$")
+plt.stem(np.abs(X))
+plt.show()
+
+plt.title("Phase of $X(k)$")
+plt.xlabel("$k$")
+plt.ylabel("$∠X(k)$")
+plt.stem(np.angle(X))
+plt.show()
+
 print(X)
