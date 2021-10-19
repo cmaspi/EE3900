727c15746589b7a64e1ae85cd54856e2e7979e0d
cmaspi <chiragmehta1010@gmail.com>
2021-10-19 11:18:27 +0530

    exam-1 updated

--- exam-1/Codes/code.py
+++ exam-1/Codes/code.py
@@ -5,7 +5,15 @@ t1=np.linspace(-1,3,5)
 
 x=[0,0,1,0,0]
 h=[0,2,1,0,0]
+plt.stem(t1,x,use_line_collection=True)
+plt.ylabel('$x$')
+plt.xlabel('$n$')
+plt.show()
 
+plt.stem(t1,h,use_line_collection=True)
+plt.ylabel('$h$')
+plt.xlabel('$n$')
+plt.show()
 y=np.convolve(x,h)
 
 # the lower limit would change to -1+(-1)=-2, the upper limit would change to 3+3=6
@@ -15,3 +23,4 @@ plt.stem(t2,y,use_line_collection=True)
 plt.ylabel('$y$')
 plt.xlabel('$n$')
 plt.show()
+# thus, the result has been verified using python
