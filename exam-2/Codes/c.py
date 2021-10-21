ce54747c42176f68f140556b4cb3ffaea0b43894
cmaspi <chiragmehta1010@gmail.com>
2021-10-21 13:55:52 +0530

    exam-2 done

---
+++ exam-2/Codes/c.py
@@ -0,0 +1,30 @@
+import matplotlib.pyplot as plt
+import matplotlib.patches as mpatches
+import matplotlib.lines as mlines
+
+fig, ax = plt.subplots(figsize = (6,6))
+
+ax.set_xlim([-3,3])
+ax.set_ylim([-3,3])
+
+pole, = plt.plot(1/2,0, 'rx',label = 'Poles')
+
+legend = plt.legend(handles =[pole], loc = 'lower right')
+fig.gca().add_artist(legend)
+
+circle = plt.Circle([0,0],1/2,color = 'green')
+fig.gca().add_artist(circle)
+
+circle = plt.Circle([0,0],1,edgecolor = 'black',fill = 0,linestyle = '--')
+fig.gca().add_artist(circle)
+
+patches = mpatches.Patch(color="green", label="ROC")
+dotted_line = mlines.Line2D([],[],color = 'black',label='Unit circle')
+dotted_line.set_linestyle('--')
+plt.legend(handles=[patches,dotted_line], loc = 'upper right')
+# ax.set_facecolor('xkcd:lime')
+
+plt.grid()
+plt.xlabel("Re")
+plt.ylabel("Im")
+plt.show()
